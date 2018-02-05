144e1e2071bc55a4c6b8fd4bc567b83b2f68baff
elevysi <starstories@Stars-iMac.fritz.box>
2018-02-05 21:20:14 +0100

    Getting Erureka to Work on OsX

--- src/main/java/com/elevysi/site/eureka/EurekaServerApplication.java
+++ src/main/java/com/elevysi/site/eureka/EurekaServerApplication.java
@@ -2,11 +2,12 @@ package com.elevysi.site.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerApplication {
+public class EurekaServerApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
